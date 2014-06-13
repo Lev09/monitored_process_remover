e3edcb23ee3455ff319f689e607d3f520c06c1f3
Levon <levon.hakopyan@google.com>
2014-06-13 17:09:53 +0400

    i add removeProcess function and make it work

--- monitoredProcessRemover.js
+++ monitoredProcessRemover.js
@@ -1,13 +1,37 @@
 var fs = require('fs');
 var zmq = require('zmq');
+var _ = require('underscore');
 
+var mainDir = '/tmp/process-manager/';
 var subscriber = zmq.socket('sub');
 
 subscriber.on('message', function(msg) {
-	var result = JSON.parse(msg.toString());
-	console.log(result);
+	var processes = JSON.parse(msg.toString());
+	console.log(processes);
+	removeProcess(processes);
 });
 
 subscriber.connect('tcp://localhost:5557');
 subscriber.subscribe('');
 console.log('listening on port: 5557');
+
+removeProcess = function(processes) {
+
+	var removePid = function(folder, pid) {
+		fs.unlink(mainDir + folder + '/' + pid, function(error) {
+			if(error) {
+				console.log(error);
+			}
+			else {
+				console.log('pid ' + pid + ' removed');
+			}
+		});	
+	};
+
+	_.each(processes, function(process) {
+		_.each(process.pids, function(pid) {
+			removePid(process.id, pid);
+		});
+	});
+	
+};
--- simple_server.js
+++ simple_server.js
@@ -2,6 +2,15 @@ var zmq = require('zmq');
 
 var publisher = zmq.socket('pub');
 
+// create folder to test  "/tmp/process-manager/1a"
+// and files named 1, 3, 12, 55
+var processes = [
+	{
+		id: '1a',
+		pids: ['1', '3', '12', '55']
+	}
+];
+
 publisher.bind('tcp://*:5557', function(error) {
 	if(error) {
 		console.log(error);
@@ -13,5 +22,5 @@ publisher.bind('tcp://*:5557', function(error) {
 });
 
 setInterval(function() {
-	publisher.send(JSON.stringify({msg: 'hello !'}));
+	publisher.send(JSON.stringify(processes));
 }, 10000);
